a3dfb644ddc19baed513717838f20dc9af286530
A Programmer <118616915+1pod-x@users.noreply.github.com>
2026-03-13 15:47:10 +0800

    one-to-one chatroom
    Year2 Project

---
+++ chatdb.php
@@ -0,0 +1,8 @@
+<?php
+DEFINE('DB_USER','p2211032');
+DEFINE('DB_PASSWORD','shdDx2WXnmytJiLE');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_NAME','p2211032');
+$dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or
+ die('Could not connect to MySQL:'.mysqli_connect_error());
+?>
