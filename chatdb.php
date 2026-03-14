95a8082f8076c98c8deb6df8a7d36d486d400315
Lisa Shan <118616915+1pod-x@users.noreply.github.com>
2026-03-14 17:22:13 +0800

    Update chatdb.php

--- chatdb.php
+++ chatdb.php
@@ -2,7 +2,7 @@
 DEFINE('DB_USER','p2211032');
 DEFINE('DB_PASSWORD','yourpassport');
 DEFINE('DB_HOST','localhost');
-DEFINE('DB_NAME','p2211032');
+DEFINE('DB_NAME','yourusername');
 $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or
  die('Could not connect to MySQL:'.mysqli_connect_error());
 ?>
