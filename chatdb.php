9a5e947f7c3fe93c2626655053b93afe57e253c3
Lisa Shan <118616915+1pod-x@users.noreply.github.com>
2026-03-14 17:21:55 +0800

    Change database password in chatdb.php
    Updated database password for security reasons.

--- chatdb.php
+++ chatdb.php
@@ -1,6 +1,6 @@
 <?php
 DEFINE('DB_USER','p2211032');
-DEFINE('DB_PASSWORD','shdDx2WXnmytJiLE');
+DEFINE('DB_PASSWORD','yourpassport');
 DEFINE('DB_HOST','localhost');
 DEFINE('DB_NAME','p2211032');
 $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or
